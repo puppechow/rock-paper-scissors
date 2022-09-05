7b06c3429e885738f71132cf0114b3d7ff53f4ed
Kendra Puppe <kjbetters@hotmail.com>
2022-09-05 11:23:49 -0600

    Setup barebones code layout Reduce pseudocode complexity

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
-/* function getComputerChoice // randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
+console.log(`Let's play Rock Paper Scissors!`);
 
-/* function getPlayerChoice // request user to input one of three options: 'rock', 'paper', 'scissors' // store input as playerSelection */
+let playerTally = 0;
+let computerTally = 0;
+let roundsRemaining = 5;
+
+/* function getComputerChoice // randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
+function getComputerChoice {
+    
+}
+
+/* assign playerSelection // request user to input one of three options: 'rock', 'paper', 'scissors' */
+let playerSelection = prompt(`What do you choose?`);
 
 /* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].`, else `You tied! [object] on [object].` */
+function playRound(playerSelection, computerSelection) {
+
+}
 
 /* function game // call playRound function // keep track of score for 5 rounds // display tally and rounds remaining after each round // return string that declares overall winner after the game is complete // `[User/Computer] wins! [playerTally] to [computerTally]`, else `It's a tie! [playerTally] to [computerTally]` */
+function game {
+
+}
