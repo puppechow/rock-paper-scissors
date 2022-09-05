0115c2ace122818fb10ab036f4a383e451421829
Kendra Puppe <kjbetters@hotmail.com>
2022-09-05 10:58:37 -0600

    Update pseudocode to incorporate ties

--- javascript.js
+++ javascript.js
@@ -4,6 +4,6 @@
 
 /* function getPlayerChoice // request user to input one of three options: 'rock', 'paper', 'scissors' // store input as playerSelection */
 
-/* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].` */
+/* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].`, else `You tied! [object] on [object].` */
 
-/* function game // call playRound function // keep track of score for 5 rounds // display tally and rounds remaining after each round // return string that declares overall winner after the game is complete // `[User/Computer] wins! [playerTally] to [computerTally]` */
+/* function game // call playRound function // keep track of score for 5 rounds // display tally and rounds remaining after each round // return string that declares overall winner after the game is complete // `[User/Computer] wins! [playerTally] to [computerTally]`, else `It's a tie! [playerTally] to [computerTally]` */
