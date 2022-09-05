bc9a4364642e98d6232e3040b6e0a01afab9b514
Kendra Puppe <kjbetters@hotmail.com>
2022-09-05 11:57:47 -0600

    Setup random computerSelection array

--- javascript.js
+++ javascript.js
@@ -4,13 +4,12 @@ let playerTally = 0;
 let computerTally = 0;
 let roundsRemaining = 5;
 
-/* function getComputerChoice // randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
-function getComputerChoice {
-    
-}
+/* randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
+var gameArray = ['rock', 'paper', 'scissors']
+let computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
 
 /* assign playerSelection // request user to input one of three options: 'rock', 'paper', 'scissors' */
-let playerSelection = prompt(`What do you choose?`);
+let playerSelection = prompt(`What do you choose: rock, paper, or scissors?`);
 
 /* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].`, else `You tied! [object] on [object].` */
 function playRound(playerSelection, computerSelection) {
@@ -18,6 +17,6 @@ function playRound(playerSelection, computerSelection) {
 }
 
 /* function game // call playRound function // keep track of score for 5 rounds // display tally and rounds remaining after each round // return string that declares overall winner after the game is complete // `[User/Computer] wins! [playerTally] to [computerTally]`, else `It's a tie! [playerTally] to [computerTally]` */
-function game {
+function game() {
 
 }
