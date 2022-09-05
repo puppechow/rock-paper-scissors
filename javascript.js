6e11408cb0d0608256ff3235ca86ea2e27081ddf
Kendra Puppe <kjbetters@hotmail.com>
2022-09-05 10:54:25 -0600

    Establish pseudocode

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/* establish rock-paper-scissors comparison // rock beats scissors // scissors beats paper // paper beats rock */
+
+/* function getComputerChoice // randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
+
+/* function getPlayerChoice // request user to input one of three options: 'rock', 'paper', 'scissors' // store input as playerSelection */
+
+/* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].` */
+
+/* function game // call playRound function // keep track of score for 5 rounds // display tally and rounds remaining after each round // return string that declares overall winner after the game is complete // `[User/Computer] wins! [playerTally] to [computerTally]` */
