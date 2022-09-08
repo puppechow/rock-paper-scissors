d1cdecffe4d18ac45540d2e2043972d2ed0d0586
Kendra Puppe <kjbetters@hotmail.com>
2022-09-08 15:07:59 -0600

    Move selection protocol inside playRound function

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
-console.log(`Let's play Rock Paper Scissors!`);
+console.log(`Let's play Rock Paper Scissors! Best of five wins. :)`);
 
 let playerTally = 0;
 let computerTally = 0;
 let roundsRemaining = 5;
 
-/* randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
-var gameArray = ['rock', 'paper', 'scissors']
-let computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
-
-/* assign playerSelection // request user to input one of three options: 'rock', 'paper', 'scissors' */
-let playerSelection = prompt(`What do you choose: rock, paper, or scissors?`);
-
 /* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].`, else `You tied! [object] on [object].` */
 function playRound(playerSelection, computerSelection) {
+    
+    /* randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
+    var gameArray = ['rock', 'paper', 'scissors']
+    let computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
+
+    /* assign playerSelection // request user to input one of three options: 'rock', 'paper', 'scissors' */
+    let playerSelection = prompt(`What do you choose: rock, paper, or scissors?`);
 
 }
 
