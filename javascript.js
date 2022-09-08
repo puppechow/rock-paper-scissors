bccd2bfcaa8a72912ee930611a39e13570d5c8f1
Kendra Puppe <kjbetters@hotmail.com>
2022-09-08 16:14:37 -0600

    Rearrange playerSelection-to-array check protocol

--- javascript.js
+++ javascript.js
@@ -2,27 +2,27 @@ console.log(`Let's play Rock Paper Scissors! Best of five wins. :)`);
 
 /* randomly return one of the following: 'rock', 'paper', 'scissors' // dub result computerSelection */
 var gameArray = ['rock', 'paper', 'scissors']
-let computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
+var computerSelection = gameArray[Math.floor(Math.random()*gameArray.length)];
 
 /* assign playerSelection // request user to input one of three options: 'rock', 'paper', 'scissors' */
-let playerSelection = prompt(`What do you choose: rock, paper, or scissors?`);
+var playerSelection = prompt(`What do you choose: rock, paper, or scissors?`);
+
+if(gameArray.indexOf(playerSelection) === -1) {
+    var playerSelection = prompt(`Oops! You didn't make a proper selection. Try again! Choose rock, paper, or scissors.`)
+  }
 
 /* function playRound // compare computerSelection with playerSelection // return string that declares result // `You [win/lose]! [Winning object] beats [losing object].`, else `You tied! [object] on [object].` */
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == `rock` || playerSelection.toLowerCase() == `paper` || playerSelection.toLowerCase() == `scissors`) {
-        if ((playerSelection.toLowerCase() == `rock` && computerSelection == `scissors`) || (playerSelection.toLowerCase() == `paper` && computerSelection == `rock`) || (playerSelection.toLowerCase() == `scissors` && computerSelection == `paper`)) {
-            /* Player Win */
-
-        } else if ((playerSelection.toLowerCase() == `rock` && computerSelection == `paper`) || (playerSelection.toLowerCase() == `paper` && computerSelection == `scissors`) || (playerSelection.toLowerCase() == `scissors` && computerSelection == `rock`)) {
-            /* Player Lose */
-
-        } else {
-            /* Player Tie */
-
-        }
+    if ((playerSelection.toLowerCase() == `rock` && computerSelection == `scissors`) || (playerSelection.toLowerCase() == `paper` && computerSelection == `rock`) || (playerSelection.toLowerCase() == `scissors` && computerSelection == `paper`)) {
+        /* Player Win */
+        return(`You win! ` + playerSelection.toUpperCase() + ` beats ` + computerSelection.toUpperCase() + `.`)
+    } else if ((playerSelection.toLowerCase() == `rock` && computerSelection == `paper`) || (playerSelection.toLowerCase() == `paper` && computerSelection == `scissors`) || (playerSelection.toLowerCase() == `scissors` && computerSelection == `rock`)) {
+        /* Player Lose */
+        return(`You lose! ` + computerSelection.toUpperCase() + ` beats ` + playerSelection.toUpperCase() + `.`)
     } else {
-        let playerSelection = prompt(`Oops! You didn't make a proper selection. Try again! Choose rock, paper, or scissors.`);
-    }
+        /* Player Tie */
+        return(`You tied! ` + playerSelection.toUpperCase() + ` on ` + computerSelection.toUpperCase() + `.`)
+        }
 }
 
 /* function game // call playRound function // keep track of score for 5 rounds // display tally and rounds remaining after each round // return string that declares overall winner after the game is complete // `[User/Computer] wins! [playerTally] to [computerTally]`, else `It's a tie! [playerTally] to [computerTally]` */
